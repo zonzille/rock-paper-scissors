5b6c6ad31ab68611349bd3098437f4784f9b0e28
zonzille <cyprien.liaudet@gmail.com>
2019-12-24 15:19:43 +0100

    ajout de la page et du code dans deux fichiers séparés

---
+++ code.js
@@ -0,0 +1,49 @@
+// partie joueur
+let playerInput = prompt("Papier, caillou ou ciseau ?", "papier");
+let playerSelection = function() {
+  return playerInput.toLowerCase();
+}
+console.log("toi : " + playerSelection())
+
+// partie ordinateur
+let elements = [
+  "papier",
+  "caillou",
+  "ciseau"
+];
+let computerSelection = Math.floor(Math.random() * elements.length);
+
+function computerSelectionFunction() {
+  switch (computerSelection) {
+    case 0: return ("papier")
+      break;
+    case 1: return ("caillou")
+      break;
+    case 2: return ("ciseau")
+      break;
+    default: return ("euh...");
+  }
+}
+console.log("l'ordinateur : " + computerSelectionFunction());
+
+// comparaison
+function playRound(playerSelection, computerSelectionFunction) {
+  if (this.playerSelection() === this.computerSelectionFunction()) {
+    return "égalité !"
+  } else if (this.playerSelection() === "caillou" && this.computerSelectionFunction() === "papier") {
+    return "défaite ! le papier enveloppe le caillou"
+  } else if (this.playerSelection() === "caillou" && this.computerSelectionFunction() === "ciseau") {
+    return "victoire ! la pierre explose les cizo"
+  } else if (this.playerSelection() === "papier" && this.computerSelectionFunction() === "caillou") {
+    return "victoire ! le papier enveloppe le caillou"
+  } else if (this.playerSelection() === "papier" && this.computerSelectionFunction() === "ciseau") {
+    return "défaite ! les ciseaux coupent le papel"
+  } else if (this.playerSelection() === "ciseau" && this.computerSelectionFunction() === "papier") {
+    return "victoire ! les ciseaux coupent le papel"
+  } else if (this.playerSelection() === "ciseau" && this.computerSelectionFunction() === "caillou") {
+    return "défaite ! la pierre explose les cizo"
+  } else {
+    return "euh......"
+  }
+}
+console.log(playRound())
